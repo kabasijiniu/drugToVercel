a707e21cf2e23ea7d21b731889f8663f41e127e6
wei <wei@qq.com>
2023-09-09 20:27:58 +0800

    test1

--- src/components/TableBase.vue
+++ src/components/TableBase.vue
@@ -1,23 +1,31 @@
 <template>
   <div class="table-wrapper">
-    <h1>{{company}}</h1>
-    <el-table :data="tableData" style="width: 100%">
+    <h2>{{company}}</h2>
+    <el-table :data="tableData" style="width: 100%" empty-text="核对无异常">
       <el-table-column prop="name" label="药品名称" />
       <el-table-column prop="code" label="医保编码" />
       <el-table-column prop="price" label="后台价格" />
       <el-table-column prop="yb_price" label="医保价格" />
-      <el-table-column prop="hou_tai_company_name" label="后台厂家" />
-      <el-table-column prop="yb_company_name" label="医保厂家" />
+      <el-table-column prop="hou_tai_company_name" label="后台厂家" v-if="isCompanyColumnVisible"/>
+      <el-table-column prop="yb_company_name" label="医保厂家" v-if="isCompanyColumnVisible"/>
       <el-table-column prop="result" label="核对结果" />
     </el-table>
   </div>
 </template>
 
 <script setup>
-import { ref ,defineProps} from 'vue'
+import {ref, defineProps, watch} from 'vue'
 const props = defineProps(['data','company']);
 const tableData = ref(props.data);
-const company = ref(props.company);
+const isCompanyColumnVisible = ref(false);
+
+watch(() => tableData, (newVal, oldVal) => {
+  if(tableData.hou_tai_company_name !== "" && tableData.yb_company_name !==""){
+    isCompanyColumnVisible.value = true;
+  }
+})
+
+
 </script>
 
 <style lang="scss" scoped>
