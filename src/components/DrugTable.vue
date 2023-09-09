53253605b895c1bd282056bdf0291023e6fa7bec
wei <wei@qq.com>
2023-09-09 11:05:04 +0800

    test

---
+++ src/components/DrugTable.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+
+    <TableBase :data="item.ret_except_drug_list" :company="item.company_name" v-for="(item,index) in tableData" :key="index"/>
+  </div>
+</template>
+
+<script setup>
+import JsonData from "../data/data.json";
+import TableBase from "./TableBase.vue";
+import {ref} from "vue";
+
+const tableData = ref(JsonData);
+console.log(JsonData);
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/components/TableBase.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="table-wrapper">
+    <h1>{{company}}</h1>
+    <el-table :data="tableData" style="width: 100%">
+
+      <el-table-column prop="name" label="药品名称" />
+      <el-table-column prop="code" label="医保编码" />
+      <el-table-column prop="price" label="后台价格" />
+      <el-table-column prop="yb_price" label="医保价格" />
+      <el-table-column prop="hou_tai_company_name" label="后台厂家" />
+      <el-table-column prop="yb_company_name" label="医保厂家" />
+      <el-table-column prop="result" label="核对结果" />
+    </el-table>
+  </div>
+</template>
+
+<script setup>
+import { ref ,defineProps} from 'vue'
+const props = defineProps(['data','company']);
+const tableData = ref(props.data);
+const company = ref(props.company);
+</script>
+
+<style lang="scss" scoped>
+
+</style>
